cc73b52a09394296599f7ae5d5b042a0f06b9953
Al-Zinan <pappurohan1100@gmail.com>
2024-12-08 11:43:19 +0600

    addeD

---
+++ src/components/service/Service.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import slider1 from "../image/blog/Chane image (1).png";
+import slider2 from "../image/blog/Chane image (2).png";
+import slider3 from "../image/blog/Chane image.png";
+import slider4 from "../image/blog/Chane image (1).png";
+
+const Service = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <p>Cupel Therapy</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Service;
