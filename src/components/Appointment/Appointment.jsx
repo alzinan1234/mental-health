a05afe4e7cf2a96b808c263b600bf753ee501461
Al-Zinan <pappurohan1100@gmail.com>
2024-12-09 19:58:19 +0600

    added

--- src/components/Appointment/Appointment.jsx
+++ src/components/Appointment/Appointment.jsx
@@ -16,7 +16,7 @@ const Appointment = () => {
             </div>
             <div className="md:flex flex-col gap-[43px]">
               <h1 className="md:text-[48px] text-[24px] font-bold leading-[72px] text-white">
-                Whenever and wherever youneed it
+                Whenever and wherever younger it
               </h1>
               <button className="md:p-[24px] p-[15px] md:w-[270px]  bg-[#FE9816] rounded-[5px] font-semibold text-[20px] text-[#fff]">
                 Book an Appointment
