624188881d8f01cba647af4cbac4147465d3b6a0
Al-Zinan <pappurohan1100@gmail.com>
2024-12-19 00:50:26 +0600

    addded

--- src/components/blogSection/Blogpart.jsx
+++ src/components/blogSection/Blogpart.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Blogpart = () => {
   return (
     <>
-      <div>this is blog part</div>
+      <div className="text-6xl font-bold uppercase text-center">
+        blog part is loading{" "}
+      </div>
     </>
   );
 };
