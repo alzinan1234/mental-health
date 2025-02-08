2c260f989a37f236dbd613ee0228b13b075775a1
johir-dev <pappurohan1100@gmail.com>
2025-02-09 00:42:37 +0600

    Add

--- src/components/branch/Branch.jsx
+++ src/components/branch/Branch.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Branch = () => {
-  return <div></div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magni dolorum
+      veritatis nemo voluptates dolor provident tempore perferendis.
+      Dignissimos, placeat.
+    </div>
+  );
 };
 
 export default Branch;
