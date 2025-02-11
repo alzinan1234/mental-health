8d7340b979f922ca7a62840a5e7594e014a3a72b
johir-dev <pappurohan1100@gmail.com>
2025-02-12 00:09:12 +0600

    added

--- src/components/branch/Branch.jsx
+++ src/components/branch/Branch.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Branch = () => {
-  return (
-    <div>
-      <h1>this is a mirpur branch </h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Branch;
